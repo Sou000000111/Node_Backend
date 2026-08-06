e5330e1f81f45c921a7aa8f042ac4203e1a1b333
Soumyajit Sinha <sinhasoumya2018@gmail.com>
2026-08-07 01:06:30 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -12,7 +12,12 @@ app.use(express.json({limit: "20kb"}))
 
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"));
-
 app.use(cookieParser())
 
-export{ app }
+// routes
+
+import userRouter from'./routes/user.routes.js'
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+export { app }
--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,5 +1,5 @@
-const ayncHandler = (requestHandler) =>{
-    (req, res, next) =>{
+const asyncHandler = (requestHandler) =>{
+     return (req, res, next) =>{
         Promise.resolve(req, res, next).catch((err) => next(err))
     }
 }
