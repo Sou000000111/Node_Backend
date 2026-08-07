9557cc0129dcd824c02304fa39f80300551690e2
Soumyajit Sinha <sinhasoumya2018@gmail.com>
2026-08-07 11:00:03 +0530

    add register controller

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -54,7 +54,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next(); // check whether the password sould need to be updated or not
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
